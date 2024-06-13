c3d159017e1bd8e8ce14cd4fa5277cb6bcfcc16c
Saimanon <146111268+Saimanon@users.noreply.github.com>
2024-06-13 22:36:26 +0300

    Update program.py
    Someone added a new line

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first Git program')
     print('And I change it every day')
     print('again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
